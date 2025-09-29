5587a79e26d636f7468bbfe7ccee4af59ea66ddf
tatababi <zyannotlion15@gmail.com>
2025-09-29 11:59:13 -0700

    erorrs

--- P3ShiuGameplayMechanics/Assets/Course Library/Scripts/PlayerController.cs
+++ P3ShiuGameplayMechanics/Assets/Course Library/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float speed = 5.0f;
     private GameObject focalPoint;
+    public bool hasPowerup = false;
+    private float powerupStrength = 15.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,6 +18,32 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * speed *  forwardInput);
+        float sideInput = Input.GetAxis("Horizontal");
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        playerRb.AddForce(focalPoint.transform.right * sideInput * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    { if (collision.gameObject.CompareTag("Enemy") && hasPowerup) 
+       
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Player collided with" + collision.gameObject.name + " with powerup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * 10, ForceMode.Impulse);
+
+
+        }
     }
 }
+
