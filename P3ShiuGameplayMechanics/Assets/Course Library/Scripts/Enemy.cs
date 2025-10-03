07c62e2e029f5651c59b71eb9fa645adaf624863
tatababi <zyannotlion15@gmail.com>
2025-10-03 11:44:32 -0700

    :(

--- P3ShiuGameplayMechanics/Assets/Course Library/Scripts/Enemy.cs
+++ P3ShiuGameplayMechanics/Assets/Course Library/Scripts/Enemy.cs
@@ -17,7 +17,8 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
-        if (transform.position.y < -10) { Destroy(gameObject); }
+        if (transform.position.y < -10) { Destroy(gameObject);
+           }
    
 
     }
--- P3ShiuGameplayMechanics/Assets/Course Library/Scripts/SpawnManager.cs
+++ P3ShiuGameplayMechanics/Assets/Course Library/Scripts/SpawnManager.cs
@@ -1,4 +1,7 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Xml.Schema;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -6,11 +9,38 @@ public class SpawnManager : MonoBehaviour
     public GameObject enemyPrefab;
     private float spawnRange = 9.0f;
     public int enemyCount;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int waveNumber = 1;
+    public GameObject powerupPrefab;
+    public List<WeightedEnemy> weightedEnemies;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created]
+    [System.Serializable]
+    public class WeightedEnemy
+    {
+        public GameObject  enemyPrefab;
+        public float weight;
+    }
+
     void Start()
     {
 
-        SpawnEnemyWave(3);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(),
+            powerupPrefab.transform.rotation);
+    }
+
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if (enemyCount == 0)
+
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+
     }
 
     void SpawnEnemyWave(int enemiesToSpawn)
@@ -18,23 +48,40 @@ public class SpawnManager : MonoBehaviour
         for (int i = 0; i < enemiesToSpawn; i++)
         {
             {
+               
+                GameObject enemyToSpawn = GetRandomWeightedEnemy();
                 Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
             }
         }
-
-
-
-        // Update is called once per frame
-
-        void Update()
+        GameObject GetRandomWeightedEnemy()
         {
-            enemyCount = FindObjectsOfType<Enemy>().Length;
-           
-            if (enemyCount == 0)
-            { SpawnEnemyWave(1); }
-        
+            float totalWeight = 0.0f;
+
+            foreach (var weightedEnemy in weightedEnemies)
+            {
+                totalWeight += weightedEnemy.weight;
+            }
+
+            float randomValue = Random.Range(0, totalWeight);
+            float cumulativeWeight = 0.0f;
+            foreach (var weightedEnemy in weightedEnemies)
+            {
+                cumulativeWeight += weightedEnemy.weight;
+                if (randomValue < cumulativeWeight)
+                {
+                    return weightedEnemy.enemyPrefab;
+                }
+            }
+            return null;
         }
-    }    private Vector3 GenerateSpawnPosition()
+    
+
+
+
+            // Update is called once per frame
+
+
+        }    private Vector3 GenerateSpawnPosition()
     {
 
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
@@ -44,6 +91,9 @@ public class SpawnManager : MonoBehaviour
 
         return randomPos;
     }
+
+
+    
 
 
 }
